7cd52dc3d2db17a72f6b71177ae87315f3e9fb92
Ali Rooholamini <ali.rooholamini.dev@gmail.com>
2022-01-21 17:12:51 +0330

    password validation added

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,6 +31,8 @@ class formValidation{
 
     emptyValidation(elem){
         let span = elem.parentElement.nextElementSibling;
+        // set color before its channge on Password Validation
+        span.style.color = "rgb(209, 21, 21)";
         if(elem.value.length === 0){
             span.textContent = "input is empty. please fill the input correctly";
         }else{
@@ -44,11 +46,11 @@ class formValidation{
             elem.nextElementSibling.style.display = "inline-block";
         }
     }
-    emailValidation(elem){
-        let span = elem.parentElement.nextElementSibling;
+    emailValidation(elem){     
         if(elem.value.length === 0){
             return;
         }
+        let span = elem.parentElement.nextElementSibling;
         if(elem.value.indexOf("@gmail.com") > -1 || elem.value.indexOf("@yahoo.com") > -1 || elem.value.indexOf("@outlook.com") > -1){
             span.textContent = "";
             elem.nextElementSibling.classList.replace("fa-exclamation-circle" , "fa-check-circle");
@@ -63,9 +65,51 @@ class formValidation{
         if(elem.value.length === 0){
             return;
         }
-        elem.onblur = event => {
-            this.rePassValidation(rePass);
+        let span = elem.parentElement.nextElementSibling;
+        let crCounter = 0;
+        if(elem.value.match(/["#" , "@" , "!" , "*" , "&" , "^" , "(" , ")" , "|" , "/" , "\\" , ";" , "$" , "_" , "~" , "." , "," , "-" , "=" , "+" , "<" , ">"]/)){
+            crCounter++;
         }
+        if(elem.value.match(/[abcdefghijklmnopqrstuvwxyz]/)){
+            crCounter++;
+        }
+        if(elem.value.match(/[ABCDEFGHIJKLMNOPQRSTUVWXYZ]/)){
+            crCounter++;
+        }
+        if(elem.value.match(/[0123456789]/)){
+            crCounter++;
+        }
+        if(elem.value.length > 6){
+            crCounter++;
+        }
+        switch(crCounter){
+            case 1 :
+                span.textContent = "Bad Password";
+                span.style.color = "rgb(209, 21, 21)";
+                break;
+            case 2 :
+                span.textContent = "Normal Password";
+                span.style.color = "yellowGreen";
+                crCounter = 0;
+                break;
+            case 3 :
+                span.textContent = "Good Password";
+                span.style.color = "lightGreen";
+                crCounter = 0;
+                break;
+            case 4 :
+                span.textContent = "Very Good Password";
+                span.style.color = "green";
+                crCounter = 0;
+                break;
+            case 5 :
+                span.textContent = "Strong Password";
+                span.style.color = "blue";
+                crCounter = 0;
+                break;
+        }
+        crCounter = 0;
+        this.rePassValidation(rePass);
     }
     rePassValidation(elem){
         if(elem.value.length === 0){
